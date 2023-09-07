9b5fea5ee6c1e672443ecabedce4b2f305ab52c6
japark <wkd5878@gmail.com>
2023-09-07 15:17:30 +0900

    Feat - 결제페이지 MVP
    - 함수 구현 -> 기능 미구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import '../styles/globals.scss'
-import type {Metadata} from 'next'
+import Link from "next/link";
 import {Inter} from 'next/font/google'
 import AuthProvider from "@/app/context/AuthProvider";
-import Link from "next/link";
 import Script from "next/script";
+import type {Metadata} from 'next'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -24,12 +24,12 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             <div>로그인 페이지 : <Link href={'http://localhost:3000/api/auth/signin/github'}>깃허브로그인</Link></div>
             <div>로그아웃 : <Link href={'http://localhost:3000/api/auth/signout/github'}>로그아웃</Link></div>
             <div>어드민 페이지 : <Link href={'/admin'}>어드민</Link></div>
-            <div>결제 페이지 : <Link href={'/payment'}>결제</Link></div>
+            <div>결제 페이지 : <Link href={'/payment'}>페이팔</Link></div>
+            <div>결제 페이지 : <Link href={'/kg'}>이니시스</Link></div>
             <div>QR 출력 페이지 : <Link href={'/qrtest'}>QR Code</Link></div>
             {children}
         </AuthProvider>
         </body>
-        <Script src="https://cdn.iamport.kr/v1/iamport.js" />
         </html>
     )
 }
--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,11 +1,91 @@
-import React from 'react';
+'use client'
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { PaypalRequestPayParams, RequestPayResponse } from "@/portone";
+import { useRouter } from "next/navigation";
 
-const Payment = () => {
+export default function Page() {
+    const [value, setValue] = useState(0);
+    const router = useRouter();
+
+    // 2. Define a submit handler.
+    const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setValue(+e.target.value);
+
+        if (!window.IMP) return;
+
+        const { IMP } = window;
+        IMP.updateLoadUIRequest("paypal-spb", {
+            pg: "paypal_v2.{MIIiasTest}",                // PG사 : https://portone.gitbook.io/docs/sdk/javascript-sdk/payrq#undefined-1 참고
+            pay_method: "paypal",                        // 결제수단
+            merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
+            amount: +e.target.value,                     // 결제금액
+            name: "PRODUCT",                             // 주문명
+            buyer_name: "홍길동",                          // 구매자 이름
+            buyer_email: 'www@ansan.ac.kr',              // 구매자 이메일
+            buyer_tel: '01012341234',                    // 구매자 전화번호
+            products: [
+                {
+                    name: "PRODUCT",
+                    code: "abcdefg",
+                    unitPrice: +e.target.value,
+                    quantity: 1,
+                },
+            ],
+            m_redirect_url: "/payment/done",
+        });
+    };
+
+    /* 3. 콜백 함수 정의하기 */
+    async function callback(response: RequestPayResponse) {
+        const { imp_uid, merchant_uid } = response;
+
+        // 결제 후 분기처리
+    }
+
+    useEffect(() => {
+        if (!window.IMP) return;
+
+        const { IMP } = window;
+
+        const data: PaypalRequestPayParams = {
+            pg: "paypal_v2.{MIIiasTest}",                // PG사 : https://portone.gitbook.io/docs/sdk/javascript-sdk/payrq#undefined-1 참고
+            pay_method: "paypal",                        // 결제수단
+            merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
+            amount: 10,                                  // 결제금액
+            name: "PRODUCT",                             // 주문명
+            buyer_first_name: "길동",
+            buyer_last_name: "홍",
+            products: [
+                {
+                    name: "PRODUCT",
+                    code: "abcdefg",
+                    unitPrice: 10,
+                    quantity: 1,
+                },
+            ],
+            m_redirect_url: "/payment/done",
+        };
+
+        IMP.init("imp********"); // 가맹점 식별코드
+        IMP.loadUI("paypal-spb", data, callback);
+    }, []);
+
     return (
-        <div>
-            Payment
-        </div>
-    );
-};
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <input
+                type="number"
+                placeholder="value"
+                onChange={onInputChange}
+                value={value}
+                className="mb-4 w-[200px]"
+            />
 
-export default Payment;
+            <div
+                className="portone-ui-container"
+                data-portone-ui-type="paypal-spb"
+            >
+                {/* <!-- 3. 여기에 페이팔 버튼이 생성됩니다. --> */}
+            </div>
+        </main>
+    );
+}
