49b74ed48dfa927db200509f1e52a09ec3fe87e5
JangHwanPark <wkd5878@gmail.com>
2023-09-08 09:24:53 +0900

    set - tailwind setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,20 +19,38 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <head>
             <title>AU App</title>
         </head>
-        <Script src="https://cdn.iamport.kr/v1/iamport.js" />
+        <Script src="https://cdn.iamport.kr/v1/iamport.js"/>
         <body className={inter.className}>
         <AuthProvider>
             <div className='mx-auto w-1/2 p-4'>
                 <h2 className='text-2xl font-bold'>임시 페이지</h2>
-                <div>메인 페이지 : <Link href={'/'}>메인</Link></div>
-                <div>로그인 페이지 : <Link href={'/login'}>로그인</Link></div>
-                <div>로그인 페이지 : <Link href={'http://localhost:3000/api/auth/signin/github'}>깃허브로그인</Link></div>
-                <div>로그아웃 : <Link href={'http://localhost:3000/api/auth/signout/github'}>로그아웃</Link></div>
-                <div>어드민 페이지 : <Link href={'/admin'}>어드민</Link></div>
-                <div>결제 페이지 : <Link href={'/payment'}>페이팔</Link></div>
-                <div>결제 페이지 : <Link href={'/kg'}>이니시스</Link></div>
-                <div>결제 페이지 : <Link href={'/v2'}>이니시스 V2</Link></div>
-                <div>QR 출력 페이지 : <Link href={'/qrtest'}>QR Code</Link></div>
+                <div>메인 페이지 :
+                    <Link href={'/'} className='text-red-400 font-bold'>메인</Link>
+                </div>
+                <div>로그인 페이지 :
+                    <Link href={'/login'} className='text-red-400 font-bold'>로그인</Link>
+                </div>
+                <div>로그인 페이지 :
+                    <Link href={'http://localhost:3000/api/auth/signin/github'} className='text-red-400 font-bold'>깃허브로그인</Link>
+                </div>
+                <div>로그아웃 :
+                    <Link href={'http://localhost:3000/api/auth/signout/github'} className='text-red-400 font-bold'>로그아웃</Link>
+                </div>
+                <div>어드민 페이지 :
+                    <Link href={'/admin'} className='text-red-400 font-bold'>어드민</Link>
+                </div>
+                <div>결제 페이지 :
+                    <Link href={'/payment' } className='text-red-400 font-bold'>페이팔</Link>
+                </div>
+                <div>결제 페이지 :
+                    <Link href={'/kg'} className='text-red-400 font-bold'>이니시스</Link>
+                </div>
+                <div>결제 페이지 :
+                    <Link href={'/v2'} className='text-red-400 font-bold'>이니시스 V2</Link>
+                </div>
+                <div>QR 출력 페이지 :
+                    <Link href={'/qrtest'} className='text-red-400 font-bold'>QR Code</Link>
+                </div>
             </div>
             {children}
         </AuthProvider>
