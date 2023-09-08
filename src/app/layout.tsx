1af9b492dfd7fe3ba15acd21ad4705c7994ed52f
JangHwanPark <wkd5878@gmail.com>
2023-09-08 09:17:59 +0900

    set - tailwind setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/globals.scss'
+import '@/app/globals.css';
 import Link from "next/link";
 import {Inter} from 'next/font/google'
 import AuthProvider from "@/app/context/AuthProvider";
@@ -22,16 +22,18 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <Script src="https://cdn.iamport.kr/v1/iamport.js" />
         <body className={inter.className}>
         <AuthProvider>
-            <h2>임시 페이지</h2>
-            <div>메인 페이지 : <Link href={'/'}>메인</Link></div>
-            <div>로그인 페이지 : <Link href={'/login'}>로그인</Link></div>
-            <div>로그인 페이지 : <Link href={'http://localhost:3000/api/auth/signin/github'}>깃허브로그인</Link></div>
-            <div>로그아웃 : <Link href={'http://localhost:3000/api/auth/signout/github'}>로그아웃</Link></div>
-            <div>어드민 페이지 : <Link href={'/admin'}>어드민</Link></div>
-            <div>결제 페이지 : <Link href={'/payment'}>페이팔</Link></div>
-            <div>결제 페이지 : <Link href={'/kg'}>이니시스</Link></div>
-            <div>결제 페이지 : <Link href={'/v2'}>이니시스 V2</Link></div>
-            <div>QR 출력 페이지 : <Link href={'/qrtest'}>QR Code</Link></div>
+            <div className='mx-auto w-1/2 p-4'>
+                <h2 className='text-2xl font-bold'>임시 페이지</h2>
+                <div>메인 페이지 : <Link href={'/'}>메인</Link></div>
+                <div>로그인 페이지 : <Link href={'/login'}>로그인</Link></div>
+                <div>로그인 페이지 : <Link href={'http://localhost:3000/api/auth/signin/github'}>깃허브로그인</Link></div>
+                <div>로그아웃 : <Link href={'http://localhost:3000/api/auth/signout/github'}>로그아웃</Link></div>
+                <div>어드민 페이지 : <Link href={'/admin'}>어드민</Link></div>
+                <div>결제 페이지 : <Link href={'/payment'}>페이팔</Link></div>
+                <div>결제 페이지 : <Link href={'/kg'}>이니시스</Link></div>
+                <div>결제 페이지 : <Link href={'/v2'}>이니시스 V2</Link></div>
+                <div>QR 출력 페이지 : <Link href={'/qrtest'}>QR Code</Link></div>
+            </div>
             {children}
         </AuthProvider>
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import styles from './page.module.scss'
 import Link from "next/link";
-import {getServerSession} from "next-auth";
 import {options} from "@/app/api/auth/[...nextauth]/option";
+import {getServerSession} from "next-auth";
 import {undefined} from "zod";
 
 type User = {
@@ -22,23 +21,24 @@ export default async function Home() {
   const session: Session | null = await getServerSession(options)
 
     return (
-    <main className={styles.main}>
-
-        <h2>세션 정보</h2>
-        <p>
-            아이디 : test<br/>
-            비밀번호 : test<br/>
-            공급자 : 깃허브
-        </p>
-        <>
-            {session ? (
-                <div>session : {session?.user?.name}</div>
-            ) : (
-                <div>session : null</div>
-            )}
-        </>
-        <h2>QR 예시</h2>
-        <div><Link href={'/qrtest'}>QR Home</Link></div>
+    <main className='mx-auto w-1/2 p-4'>
+        <div>
+            <h2>세션 정보</h2>
+            <p>
+                아이디 : test<br/>
+                비밀번호 : test<br/>
+                공급자 : 깃허브
+            </p>
+            <>
+                {session ? (
+                    <div>session : {session?.user?.name}</div>
+                ) : (
+                    <div>session : null</div>
+                )}
+            </>
+            <h2>QR 예시</h2>
+            <div><Link href={'/qrtest'}>QR Home</Link></div>
+        </div>
     </main>
   )
 }
