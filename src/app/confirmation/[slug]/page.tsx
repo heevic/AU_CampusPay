adb755a948c1f4ede6fba9b8914bf53c9caacb3e
JangHwanPark <wkd5878@gmail.com>
2023-09-19 01:54:25 +0900

    - 결제 히스토리 디자인

--- src/app/confirmation/[slug]/page.tsx
+++ src/app/confirmation/[slug]/page.tsx
@@ -21,9 +21,9 @@ const ConfirmationPage = async ({params}: Props) => {
                 <p className="text-center text-gray-700 font-medium">
                     {params.slug}
                     <br />
-                    중식 n식권
+                    중식
                     <br />
-                    사용여부
+                    미사용
                 </p>
                 <button className="mt-6 px-8 py-2 bg-red-500 text-white rounded-full font-medium tracking-wide hover:bg-red-600 transition ease-in-out duration-300">
                     취소요청
